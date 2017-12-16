e629bd991a87cd6c849fcb726f49a0edcbaeb2c1
Will Kim <rlagmlckd@gmail.com>
2017-12-16 13:53:33 -0600

    Initial Commit

---
+++ whoIsWill.js
@@ -0,0 +1,25 @@
+var i = 0;
+
+function whoIsWill() {
+  console.log('This is Will');
+  // Will;
+
+  // Korean Born;
+  // Lived in Kuwait;
+  // British School;
+  // Art School;
+  // Jesus Follower;
+  // Invely's;
+  // withoutwax.me
+  // Chicago;
+}
+
+function Will() {
+  whoIsWill();
+  i++;
+  if (i < 20) {
+    Will();
+  }
+}
+
+Will();
