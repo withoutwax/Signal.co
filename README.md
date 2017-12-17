0c9eafad6b2c99b36a418b47764cd4c743b11cab
Will Kim <rlagmlckd@gmail.com>
2017-12-16 18:50:28 -0600

    150 Words

---
+++ README.md
@@ -0,0 +1,5 @@
+# Signal.co  
+
+This is very simple project which I decided to tackle while applying to Front-End Engineering Position at Signal.co (without API)
+
+WIP
--- whoIsWill.js
+++ whoIsWill.js
@@ -3,6 +3,15 @@ var i = 0;
 function whoIsWill() {
   console.log('This is Will');
   // Will;
+
+  // Korean Born;
+  // Lived in Kuwait;
+  // Chicago;
+  // British School;
+  // Art School;
+  // Jesus Follower;
+  // Invely's;
+  // withoutwax.me
 }
 
 function Will() {
